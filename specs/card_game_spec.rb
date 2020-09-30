7720bfc4efc93d25f26b95328e5d4c5cca7f839a
James Monks <J.monks23@gmail.com>
2020-09-30 18:32:28 +0100

    added screenshots of tests failing and passing once code had been changed to match the testing task 1 comments

--- specs/card_game_spec.rb
+++ specs/card_game_spec.rb
@@ -9,26 +9,26 @@ require_relative( "../card_game" )
 class CardGameTest < MiniTest::Test
 
     def setup()
-        @card_1 = Card.new("Spades", 1)
-        @card_2 = Card.new("Queen", 6)
-        @card_3 = Card.new("Clubs", 7)
+        @card1 = Card.new("Spades", 1)
+        @card2 = Card.new("Queen", 7)
+        @card3 = Card.new("Clubs", 8)
     end
 
     def test_check_for_ace_true()
-        assert(CardGame.check_for_ace(@card_1))
+        assert(CardGame.check_for_ace(@card1))
     end
 
     def test_check_for_ace_false()
-        refute(CardGame.check_for_ace(@card_2))
+        refute(CardGame.check_for_ace(@card2))
     end
 
     def test_highest_card()
-        result = CardGame.highest_card(@card_2, @card_3)
-        assert_equal(result, @card_3)
+        result = CardGame.highest_card(@card2, @card3)
+        assert_equal(result, @card3)
     end
 
     def test_cards_total()
-        cards = [@card_2, @card_3]
+        cards = [@card2, @card3]
         result = CardGame.cards_total(cards)
         assert_equal(result, "You have a total of 15")
     end
